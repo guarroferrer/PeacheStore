efd83b11bcd4b808c7d989f487d8e971fd73f74b
guarroferrer <marsal_1991@hotmail.com>
2014-05-16 17:11:47 +0200

    update main

--- main.py
+++ main.py
@@ -2,3 +2,13 @@ def listar():
      with open('aplicaciones.txt',mode='r',encoding='utf-8')as archivo:
          for linia in archivo:
              print(linia)
+def añadirApp(nombre,proveedor,precio,fecha):
+
+
+    identificaion=(nombre+","+proveedor+","+fecha)
+    numeroDes="0"
+    numeroPunt="0"
+    puntuacion="0"
+    numComen="0"
+    with open('aplicaciones.txt',mode='a',encoding='utf-8')as archivo:
+        archivo.write(identificaion+","+precio+","+numeroDes+","+numeroPunt+","+puntuacion+","+numComen+"\n")
