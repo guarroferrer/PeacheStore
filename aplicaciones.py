5040d2f0e0bee5536974f69105a46d7f761e89f4
guarroferrer <marsal_1991@hotmail.com>
2014-05-16 17:34:23 +0200

    Update aplicaciones.py

--- aplicaciones.py
+++ aplicaciones.py
@@ -1,1 +1,16 @@
 #fichero aplicaciones
+__author__ = 'Alumne'
+class Aplicaciones():
+    def __init__(self,nombre,proveedor,fecha,precio):
+        self.fecha = fecha
+        self.nombre= nombre
+        self.proveedor=proveedor
+        self.precio=precio
+    def getFecha(self):
+        return self.fecha
+    def getNombre(self):
+        return self.nombre
+    def getPrecio(self):
+        return self.precio
+    def getproveedor(self):
+        return self.proveedor
